3f80163c5b4eb86620a1a17b0e9f9124c8162941
ahmedkorim <ahmedkorrim@gmail.com>
2018-11-02 11:13:25 +0200

    simple attack

--- app.js
+++ app.js
@@ -13,13 +13,22 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack() {
-
+            let maxDam = 10;
+            let minDam = 3;
+            let damage = Math.max(Math.floor(Math.random() * maxDam) + 1, minDam);
+            this.monsterHealth -= damage;
+             maxDam = 12;
+             minDam = 5;
+             damage = Math.max(Math.floor(Math.random() * maxDam) + 1, minDam);
+            this.playerHealth -= damage;
         },
         heal() {
 
         },
         giveUp() {
-
+            this.gameIsRunning = false;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
         },
         specialAttack() {
 
